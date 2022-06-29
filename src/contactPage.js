3f4e8b39e4fdb35b4e713f2ae30fa010f707c872
AlexSaxena <saxenaalex@gmail.com>
2022-06-29 17:18:31 +0200

    Adds Create Form Function & main Wrapper elements

---
+++ src/contactPage.js
@@ -0,0 +1,123 @@
+// Btn prevent submit
+// document
+//   .getElementById("contact-btn")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault();
+//   });
+
+function renderContactPage() {
+  let mainDiv = document.createElement("main");
+  mainDiv.className = "main-contact";
+  let contactWrapper = document.createElement("div");
+  contactWrapper.className = "contact-wrapper";
+  let formWrapper = document.createElement("div");
+  formWrapper.className = "form-wrapper";
+  let formIntro = document.createElement("h1");
+  formIntro.className = "form-intro";
+  formIntro.textContent = "Feedback";
+  let newForm = createForm();
+ let 
+  return mainDiv;
+}
+
+function createForm() {
+  let contactForm = document.createElement("form");
+  contactForm.setAttribute("method", "post");
+  contactForm.setAttribute("action", "#");
+
+  //Labels for all Form Elements
+  let labelFname = document.createElement("label");
+  labelFname.setAttribute("for", "fname");
+  labelFname.innerHTML = "First Name";
+
+  let labelLname = document.createElement("label");
+  labelLname.setAttribute("for", "lname");
+  labelLname.innerHTML = "Last Name";
+
+  let labelEmail = document.createElement("label");
+  labelEmail.setAttribute("for", "email");
+  labelEmail.innerHTML = "E-Mail";
+
+  let labelTelnr = document.createElement("label");
+  labelTelnr.setAttribute("for", "telNumber");
+  labelTelnr.innerHTML = "Telephone Number";
+
+  let labelCategory = document.createElement("label");
+  labelCategory.setAttribute("for", "Category");
+  labelCategory.innerHTML = "Category";
+
+  // Create an input element for First Name
+  let inputFName = document.createElement("input");
+  inputFName.setAttribute("type", "text");
+  inputFName.setAttribute("id", "fname");
+  inputFName.setAttribute("name", "firstname");
+  inputFName.setAttribute("placeholder", "Your name..");
+  inputFName.setAttribute("required", "");
+
+  // Create an input element for Last Name
+  let inputLName = document.createElement("input");
+  inputLName.setAttribute("type", "text");
+  inputLName.setAttribute("id", "lname");
+  inputLName.setAttribute("name", "lastname");
+  inputLName.setAttribute("placeholder", "Your last name..");
+  inputLName.setAttribute("required", "");
+
+  // Create an input element for E-Mail
+  let inpuEmail = document.createElement("input");
+  inpuEmail.setAttribute("type", "email");
+  inpuEmail.setAttribute("id", "email");
+  inpuEmail.setAttribute("name", "emailAdress");
+  inpuEmail.setAttribute("placeholder", "BurgerLover@mail.com");
+  inpuEmail.setAttribute("required", "");
+
+  // Create an input element for Tel-Number
+  let inputTelnr = document.createElement("input");
+  inputTelnr.setAttribute("type", "tel");
+  inputTelnr.setAttribute("id", "telNumber");
+  inputTelnr.setAttribute("name", "telephoneNumber");
+  inputTelnr.setAttribute("placeholder", "+123-45678910");
+
+  // Create an Select element for Category
+  let inputCategory = document.createElement("select");
+  inputCategory.setAttribute("id", "Category");
+  inputCategory.setAttribute("name", "Category");
+
+  let opt1 = document.createElement("option");
+  let opt2 = document.createElement("option");
+  let opt3 = document.createElement("option");
+
+  opt1.value = "food&beverage";
+  opt1.text = "Food & Beverage";
+
+  opt2.value = "service";
+  opt2.text = "Service";
+
+  opt3.value = "other";
+  opt3.text = "Other";
+
+  inputCategory.add(opt1, null);
+  inputCategory.add(opt2, null);
+  inputCategory.add(opt3, null);
+
+  // Create a submit button
+  let btnSubmitForm = document.createElement("input");
+  btnSubmitForm.setAttribute("type", "submit");
+  btnSubmitForm.setAttribute("value", "Submit");
+
+  //Append Form
+  contactForm.append(
+    labelFname,
+    inputFName,
+    labelLname,
+    inputLName,
+    labelEmail,
+    inpuEmail,
+    labelTelnr,
+    inputTelnr,
+    labelCategory,
+    inputCategory,
+    btnSubmitForm
+  );
+
+  return contactForm;
+}
