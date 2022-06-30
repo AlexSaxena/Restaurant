cdb67b511922e253a3a234cbef59f38ccde829c7
AlexSaxena <saxenaalex@gmail.com>
2022-06-30 15:52:34 +0200

    Adds Click EventListeners for Page changing

--- src/index.js
+++ src/index.js
@@ -9,4 +9,32 @@ console.log(123);
 
 let contentDiv = document.querySelector(".content-div");
 
-contentDiv.append(renderHeader(), renderContactPage(), renderFooter());
+contentDiv.append(renderHeader(), renderIntroPage(), renderFooter());
+
+let mainDiv = contentDiv.children[1];
+
+// All the Nav Btns
+let btnHome = document.querySelector(".btn-home");
+let btnMenu = document.querySelector(".btn-menu");
+let btnContact = document.querySelector(".btn-contact");
+let btnContactUs = document.querySelector(".btn-contact-us");
+
+btnHome.addEventListener("click", () => {
+  contentDiv.replaceChild(renderIntroPage(), mainDiv);
+  mainDiv = contentDiv.children[1];
+});
+
+btnMenu.addEventListener("click", () => {
+  contentDiv.replaceChild(renderMenuPage(), mainDiv);
+  mainDiv = contentDiv.children[1];
+});
+
+btnContact.addEventListener("click", () => {
+  contentDiv.replaceChild(renderContactPage(), mainDiv);
+  mainDiv = contentDiv.children[1];
+});
+
+btnContactUs.addEventListener("click", () => {
+  contentDiv.replaceChild(renderContactPage(), mainDiv);
+  mainDiv = contentDiv.children[1];
+});
